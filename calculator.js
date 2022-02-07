b9698e904a2dda4e5892ce305c3402485b95c0a8
Yosua Nicolaus <cryodawn257@gmail.com>
2022-02-08 00:29:44 +0700

    add event listener to buttons

--- calculator.js
+++ calculator.js
@@ -4,3 +4,16 @@ const multiply  = (a, b) => a * b;
 const divide    = (a, b) => a / b;
 
 const operate = (operator, a, b) => operator(a, b);
+
+const disBeta = document.getElementById('disBeta');
+const disMain = document.getElementById('disMain');
+const buttons = document.querySelectorAll('button');
+
+disBeta.textContent += 1000;
+disMain.textContent += 20;
+
+// button.addEventListener('click', myFunc(button.id));
+
+buttons.forEach((button) => {
+    button.addEventListener('click', () => console.log(button.id));
+});
